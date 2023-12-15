7cd0cee37afd11a2f95fb5ea03853cb13c8a59ee
sajib1066 <sajib1066@gmail.com>
2023-12-16 01:13:53 +0800

    update bongochat utils package

--- logger/logger.go
+++ logger/logger.go
@@ -1,18 +1,36 @@
 package logger
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	Log *zap.Logger
+const (
+	envLogLevel  = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
 )
+
+var (
+	log logger
+)
+
+type bookstoreLogger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
 
 func init() {
 	logConfig := zap.Config{
-		OutputPaths: []string{"stdout", "./logs/debug.log"},
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
 		Encoding:    "json",
 		EncoderConfig: zapcore.EncoderConfig{
 			LevelKey:     "level",
@@ -25,21 +43,55 @@ func init() {
 	}
 
 	var err error
-	if Log, err = logConfig.Build(); err != nil {
+	if log.log, err = logConfig.Build(); err != nil {
 		panic(err)
 	}
+}
 
+func getLevel() zapcore.Level {
+	switch strings.ToLower(strings.TrimSpace(os.Getenv(envLogLevel))) {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "error":
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func getOutput() string {
+	output := strings.TrimSpace(os.Getenv(envLogOutput))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
+func GetLogger() bookstoreLogger {
+	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func (l logger) Print(v ...interface{}) {
+	Info(fmt.Sprintf("%v", v))
 }
 
 func Info(msg string, tags ...zap.Field) {
-	Log.Info(msg, tags...)
-	Log.Sync()
+	log.log.Info(msg, tags...)
+	log.log.Sync()
 }
 
 func Error(msg string, err error, tags ...zap.Field) {
-	if err != nil {
-		tags = append(tags, zap.NamedError("error", err))
-	}
-	Log.Error(msg, tags...)
-	Log.Sync()
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Error(msg, tags...)
+	log.log.Sync()
 }
