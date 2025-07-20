3a934894967ea475a4afb50a2590f8be6ba35d0b
sajib1066 <sajib1066@gmail.com>
2025-07-21 03:32:01 +0800

    add date utils

--- date/date.go
+++ date/date.go
@@ -3,6 +3,8 @@ package date
 import (
 	"fmt"
 	"time"
+
+	"github.com/bongochat/utils/resterrors"
 )
 
 const (
@@ -45,4 +47,45 @@ func TimeAgo(datetimeStr string) string {
 	default:
 		return fmt.Sprintf("%dy", int(duration.Hours()/(24*365)))
 	}
+}
+
+// DateFormat takes a date string in any common format and returns it in DD-MM-YYYY format.
+func DateFormat(dateStr string) (string, resterrors.RestError) {
+	// List of possible date formats
+	formats := []string{
+		"2006-01-02",      // YYYY-MM-DD
+		"02-01-2006",      // DD-MM-YYYY
+		"01/02/2006",      // MM/DD/YYYY
+		"02/01/2006",      // DD/MM/YYYY
+		"2006/01/02",      // YYYY/MM/DD
+		"January 2, 2006", // Month Day, Year
+		"2 January 2006",  // Day Month Year
+		"02 Jan 2006",     // DD Mon YYYY
+		"Jan 2, 2006",     // Mon Day, Year
+		time.RFC3339,      // RFC3339
+		time.RFC1123,      // RFC1123
+		time.RFC1123Z,     // RFC1123Z
+		time.RFC850,       // RFC850
+		time.ANSIC,        // ANSIC
+		time.UnixDate,     // UnixDate
+		time.RubyDate,     // RubyDate
+	}
+
+	var parsedDate time.Time
+	var err error
+
+	// Try parsing the date string with each format
+	for _, format := range formats {
+		parsedDate, err = time.Parse(format, dateStr)
+		if err == nil {
+			break
+		}
+	}
+
+	if err != nil {
+		return "", resterrors.NewInternalServerError("Invalid date format", "", err)
+	}
+
+	// Return the date in DD-MM-YYYY format
+	return parsedDate.Format("02-01-2006"), nil
 }
